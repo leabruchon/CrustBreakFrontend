498baadbfebfefdd4f08d59881eea10776ef845f
Tibserre <thibault.serre@epfedu.fr>
2022-05-13 10:43:12 +0200

    Init Carde Recipe

---
+++ src/components/CardRecipe.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Card">
+    <div class="Img"></div>
+    <div class="BottomCard">
+      <!--
+      Label puis icon
+      -->
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CardRecipe",
+});
+</script>
