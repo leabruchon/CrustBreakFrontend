15e070017677bb49dca28ee21d4e4da1a4f7c202
BaptisteZloch <bzloch@hotmail.fr>
2022-06-13 12:27:22 +0200

    data de l'PAI dans details

--- src/pages/RecipePage.vue
+++ src/pages/RecipePage.vue
@@ -1,38 +1,48 @@
 <template>
   <q-page class="flex flex-center">
-    <IntroRecipe 
-    title = "Nom de la recette un peu long" 
-    image = "imageRecipe.jpeg"
-    time = 9
-    nbPerson = 3
+    <IntroRecipe
+      title="Nom de la recette un peu long"
+      image="imageRecipe.jpeg"
+      time="9"
+      nbPerson="3"
     />
 
-    <StepRecipe 
-    title = "Step 1"
-    consigne = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis eu at lacus aliquam amet libero. Vulputate est non diam cursus malesuada. Eget montes.
+    <StepRecipe
+      title="Step 1"
+      consigne="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis eu at lacus aliquam amet libero. Vulputate est non diam cursus malesuada. Eget montes.
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis eu at lacus aliquam amet libero. Vulputate est non diam cursus malesuada. Eget montes."
-    
     />
 
-    <StepRecipe 
-    title = "Step 2"
-    consigne = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis eu at lacus aliquam amet libero. Vulputate est non diam cursus malesuada. Eget montes.
+    <StepRecipe
+      title="Step 2"
+      consigne="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis eu at lacus aliquam amet libero. Vulputate est non diam cursus malesuada. Eget montes.
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis eu at lacus aliquam amet libero. Vulputate est non diam cursus malesuada. Eget montes."
-    
     />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-import IntroRecipe from 'src/components/IntroRecipe.vue'
-import StepRecipe from 'src/components/StepRecipe.vue'
+import { defineComponent } from "vue";
+import IntroRecipe from "src/components/IntroRecipe.vue";
+import StepRecipe from "src/components/StepRecipe.vue";
+import { api } from "../utils/api.js";
 
 export default defineComponent({
-  name: 'RecipePage', 
+  name: "RecipePage",
   components: {
-      IntroRecipe,
-      StepRecipe
-  }
-})
+    IntroRecipe,
+    StepRecipe,
+  },
+  data() {
+    return {
+      recette_detaillee: null,
+    };
+  },
+  async mounted() {
+    const API = new api();
+    const recette = await API.getRecipeDetails(479101);
+    this.recette_detaillee = recette;
+    //console.log(this.recette_detaillee._title);
+  },
+});
 </script>
