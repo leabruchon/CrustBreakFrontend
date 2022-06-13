a68865d1d4d89d444e8afc896d50e771a14fafa5
Tibserre <thibault.serre@epfedu.fr>
2022-06-13 12:02:07 +0200

    Commit add reciepe empty

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex flex-center PageIndex">
     <div class="top-bar">
       <SearchBar />
       <FilterButton />
@@ -9,13 +9,73 @@
       <SpinnerGenerateRecipe @NbRecette="TakeNbRecFromChild" />
     </div>
     <div class="recipes">
-      <CardRecipe @AddToLike="ChangeStateLike" />
+      <div class="rowReciepes">
+        <div class="reciepe">
+          <CardRecipe
+            @AddToLike="ChangeStateLike"
+            :RecetteID="rID1"
+            :RecetteTitle="rN1"
+            :RecetteImg="rImg1"
+            :RecetteLiked="rL1"
+          />
+        </div>
+        <div class="reciepe">
+          <CardRecipe
+            @AddToLike="ChangeStateLike"
+            :RecetteID="rID2"
+            :RecetteTitle="rN2"
+            :RecetteImg="rImg2"
+            :RecetteLiked="rL2"
+          />
+        </div>
+      </div>
+      <div class="rowReciepes">
+        <div class="reciepe">
+          <CardRecipe
+            @AddToLike="ChangeStateLike"
+            :RecetteID="rID3"
+            :RecetteTitle="rN3"
+            :RecetteImg="rImg3"
+            :RecetteLiked="rL3"
+          />
+        </div>
+        <div class="reciepe">
+          <CardRecipe
+            @AddToLike="ChangeStateLike"
+            :RecetteID="rID4"
+            :RecetteTitle="rN4"
+            :RecetteImg="rImg4"
+            :RecetteLiked="rL4"
+          />
+        </div>
+      </div>
+      <div class="rowReciepes">
+        <div class="reciepe">
+          <CardRecipe
+            @AddToLike="ChangeStateLike"
+            :RecetteID="rID5"
+            :RecetteTitle="rN5"
+            :RecetteImg="rImg5"
+            :RecetteLiked="rL5"
+          />
+        </div>
+        <div class="reciepe">
+          <CardRecipe
+            @AddToLike="ChangeStateLike"
+            :RecetteID="rID6"
+            :RecetteTitle="rN6"
+            :RecetteImg="rImg6"
+            :RecetteLiked="rL6"
+          />
+        </div>
+      </div>
     </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+window.onload = function () {};
+import { defineComponent, onMounted } from "vue";
 import SpinnerGenerateRecipe from "src/components/SpinnerGenerateRecipe.vue";
 import FilterButton from "src/components/FilterButton.vue";
 import SearchBar from "src/components/SearchBar.vue";
@@ -25,6 +85,10 @@ export default defineComponent({
   name: "IndexPage",
 
   methods: {
+    LoadSixStations() {
+      return "test";
+    },
+
     ChangeStateLike(IdRecette) {
       console.log("LogParent Id Recette : " + IdRecette);
     },
@@ -32,6 +96,8 @@ export default defineComponent({
       console.log("LogParent Nb Recettes : " + NbRecChild);
     },
   },
+
+  data() {},
 
   components: {
     FilterButton,
@@ -43,16 +109,36 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+.PageIndex {
+  align-content: flex-start;
+}
 .top-bar {
   display: flex;
   width: 100%;
   align-items: center;
+  align-content: flex-start;
   justify-content: space-evenly;
+  margin-top: 18px;
 }
 
 .spinner-generation {
   display: flex;
   justify-content: center;
+  margin: 24px;
+}
+
+.recipes {
+  display: flex;
+  flex-direction: column;
+}
+
+.rowReciepes {
+  display: flex;
+  flex-direction: row;
+}
+
+.reciepe {
+  margin: 2%;
 }
 
 .user {
